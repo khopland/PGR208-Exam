9f8b23bb99cbd907eb7914409d7c70c7cabd78db
Kristoffer <24739853+97krihop@users.noreply.github.com>
2021-04-10 13:51:13 +0200

    added room and Entitys

---
+++ app/src/main/java/com/example/pgr208_2021_android_exam/db/db/Database.kt
@@ -0,0 +1,10 @@
+package com.example.pgr208_2021_android_exam.db.db
+
+import androidx.room.RoomDatabase
+
+
+const val DATABASE_NAME: String = "stonks_database"
+
+abstract class Database : RoomDatabase() {
+
+}
